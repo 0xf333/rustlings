14fc32ac1c9264b7f6ecf8327873194ae0e85ab4
0xf333 <0x333@tuta.io>
2023-02-13 06:28:43 -0500

    Quiz number 1 | Status:  completed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i8) -> i8{
+   
+    if amount > 40 {
+        return amount;
+
+    } else {
+        return 2 * amount;
+    }  
+}
 
 // Don't modify this function!
 #[test]
